bd8e7e0feb79d71122dcc1046a5869b2ac771e3d
bopes <clcblee@gmail.com>
2016-06-14 16:35:24 -0700

    Add invalid login message and test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,7 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
-    assert_select 'div.alert_danger'
+    assert_select 'div.alert-danger'
   end
 
 end
