b354ec151aed3bf3f9faeebe6945a009f7cb0da7
bopes <clcblee@gmail.com>
2016-06-13 18:43:49 -0700

    Add stretch tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,10 +9,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign Up")
   end
 
-  test "signup route" do
-    get signup_path
-    assert_template 'users/new'
-  end
 end
