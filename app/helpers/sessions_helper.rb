4c1a824ae276cdcfe38a9bca8453caa615c5800d
bopes <clcblee@gmail.com>
2016-06-15 12:47:51 -0700

    Add remember me validation to current_user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,15 +11,21 @@ module SessionsHelper
   end
 
   def current_user
-    if (user_id = sessions[:user_id])
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(coookies[:remember_token])
-        log_in
-        user_id@current_user = user
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
     end
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   def logged_in?
@@ -27,6 +33,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete(:user_id)
     @current_user = nil
   end
