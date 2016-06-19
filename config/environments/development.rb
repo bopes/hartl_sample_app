5dc1f467783516d7bdfd547dd4a528dce76e0a84
bopes <clcblee@gmail.com>
2016-06-19 08:13:48 -0700

    Update email host to localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.active_support.deprecation = :log
