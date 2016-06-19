b192039cd9a2ada005503c04bb16bb41839370c4
bopes <clcblee@gmail.com>
2016-06-19 07:45:13 -0700

    Add activation token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
 
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -44,5 +45,15 @@ class User < ActiveRecord::Base
 
   end
 
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
