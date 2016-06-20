a0d0facea279e857c7d367fb1b41fd05a10b8a73
bopes <clcblee@gmail.com>
2016-06-20 13:55:29 -0700

    Add integration test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'micropost interface' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_no_difference 'Micropost.count' do # invalid submission
+      post microposts_path, micropost: { content: "" }
+    end
+    assert_select 'div#error_explanation'
+    content = "This micropost really ties the room together"
+    assert_difference 'Micropost.count', 1 do # valid submission
+      post microposts_path, micropost: { content: content }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    assert_select 'a', test: 'delete' # delete a post
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    get user_path(users(:archer)) # visit different user
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
