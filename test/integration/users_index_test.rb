8dd0873c513ddaedc9fc64fc4b11e5570ebecedb
bopes <clcblee@gmail.com>
2016-06-19 23:10:25 -0700

    Add test for nonactivated users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @non_activated = users(:cyril)
   end
 
   test 'index include pagination and delete links' do
@@ -12,7 +13,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -28,6 +29,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test 'no access to non-activated users' do
+    log_in_as(@admin)
+    get users_path
+    assert_select 'a[href=?]', user_path(@non_activated), count: 0
+    get user_path(@non_activated)
+    assert_redirected_to root_url
   end
 
 end
