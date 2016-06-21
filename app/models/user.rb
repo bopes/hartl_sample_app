f8721731acbc2f92a9c6ba9b48e5d0e6ee2246a8
bopes <clcblee@gmail.com>
2016-06-21 11:16:41 -0700

    Add following methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,6 +63,19 @@ class User < ActiveRecord::Base
   end
 
 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
   class << self
 
     def digest(string)
